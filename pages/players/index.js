361b105bc7210c1776a2b2e7296bbe248755dda0
Zwelc <tdk661@gmail.com>
2021-09-21 17:47:21 +0200

    added search component

--- pages/players/index.js
+++ pages/players/index.js
@@ -1,17 +1,57 @@
-import styles from '../../styles/Player.module.scss';
-import Link from 'next/link'
+import styles from "../../styles/Player.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
 function Players() {
-	return(
-	<div className={styles.main}>
-	<div>
-		<input type="search" />
-		<button>Search</button>
-		<Link href="/players/170365079">
-			My Personal Profile
-		</Link>
-	</div>
-</div>)
+  const baseUrl = "https://api.opendota.com/api/search?q=";
+
+  const [players, setPlayers] = useState([]);
+
+  async function handleClick() {
+    const query = document.getElementById("search").value;
+    fetch(baseUrl + query)
+      .then((res) => res.json())
+      .then((results) => setPlayers(results));
+  }
+
+  function Results() {
+    return players.map((player) => {
+      <Link href={player.account_id} key={player.account_id}>
+        <div className={styles.card}>
+          <Image src={player.avatarfull} width={80} height={68} />
+          {player.personaname}
+        </div>
+      </Link>;
+    });
+  }
+  return (
+    <div className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.searchbar}>
+          <input type="search" className={styles.search} id="search" />
+          <button className={styles.button} onClick={() => handleClick()}>
+            Search
+          </button>
+        </div>
+        <div className={styles.container}>
+          <div className={styles.results}>
+            {players.map((player) => (
+              <Link
+                href={`/players/${player.account_id}`}
+                key={player.account_id}
+              >
+                <div className={styles.card}>
+                  <Image src={player.avatarfull} width={80} height={68} />
+                  {player.personaname}
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Players;
