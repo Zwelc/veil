ab4b93728fa0931bb867b7e36c4760949da86883
Zwelc <tdk661@gmail.com>
2021-09-22 15:16:48 +0200

    updated home and matches pages to include public matches by default

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,31 +1,30 @@
-import Link from 'next/link'
-import styles from '../styles/Nav.module.scss'
+import Link from "next/link";
+import styles from "../styles/Nav.module.scss";
 
 function Sidebar() {
-	return (
-		<div className={styles.sidebar}>
-			<a>Reactive Stats</a>
+  return (
+    <div className={styles.sidebar}>
+      <a>Reactive Stats</a>
 
-			<div className={styles.wrapper}>
-				<div className={styles.title}>MENU</div>
-				<div className={styles.menu}>
-				<Link href="/">
-					<a>Home</a>
-				</Link>
-				<Link href="/players">
-					<a>Players</a>
-				</Link>
-				<Link href="/matches">
-					<a>Matches</a>
-				</Link>
-				<Link href="/heroes">
-					<a>Heroes</a>
-				</Link>
-				</div>
-			</div>
-		
-		</div>
-	)
+      <div className={styles.wrapper}>
+        <div className={styles.title}>MENU</div>
+        <div className={styles.menu}>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/players">
+            <a>Players</a>
+          </Link>
+          <Link href="/matches">
+            <a>Matches</a>
+          </Link>
+          <Link href="https://www.dota2.com/home">
+            <a target="_blank">Dota 2</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import styles from '../styles/Home.module.scss'
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.scss";
 
-export default function Home() {
+export default function Home({ matches }) {
   return (
     <>
       <Head>
@@ -10,13 +10,31 @@ export default function Home() {
         <meta name="description" content="Dota 2 overview" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-			<div className={styles.main}>
-				<div className={styles.container}>
-					<h1>Discover</h1>
-
-					
-				</div>
-			</div>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <h3>Public Matches</h3>
+          <div className={styles.grid}>
+            {matches.map((match) => (
+              <div className={styles.card} key={match.match_id}>
+                <div className={styles.headline}>{match.match_id}</div>
+                <div className={styles.average}>MMR: {match.avg_mmr}</div>
+                <div className={styles.time}>Game Time: {match.duration}</div>
+                <div className={styles.score}>
+                  Score: {match.radiant_score} : {match.dire_score}{" "}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
+}
+
+export async function getServerSideProps() {
+  const publicMatches = await fetch(
+    "https://api.opendota.com/api/publicMatches"
+  );
+  const matches = await publicMatches.json();
+  return { props: { matches } };
 }
