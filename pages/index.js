42ec2daed0fc6910156aaa833600f98e578a75b5
Thurston De Kock <56585531+Zwelc@users.noreply.github.com>
2021-10-16 19:03:40 +0200

    Removed unused sections

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,37 @@ import Head from "next/head";
 import Link from "next/link";
 
 import { Typography, Container } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+
+  const [account, setAccount] = useState(null);
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    if (localStorage) {
+      const selected = localStorage.getItem("selected");
+      if (selected) router.push(`/players/${selected}`);
+    }
+  }, [router]);
+
+  if (!account)
+    return (
+      <>
+        <Head>
+          <title>Reactive Stats</title>
+          <meta name="description" content="Dota 2 overview" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Container sx={{ mt: 4 }}>
+          <Typography variant="h6" component="text">
+            Stored Profiles
+          </Typography>
+        </Container>
+      </>
+    );
   return (
     <>
       <Head>
@@ -11,18 +40,32 @@ export default function Home() {
         <meta name="description" content="Dota 2 overview" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-			<Container sx={{mt: 4}}>
-
-			<Typography variant="h3" component="h3">
-					Reactive Stats is a small platform built for personal use that provides a quick overview of player stats as well as information of the various heroes found in the Dota 2.
-				</Typography>
-        <Typography variant="body" component="p">
-					This is a passion project inspired by <a href="https://www.dotabuff.com/" target="_blank" rel="noreferrer">DotaBuff</a> and <a href="https://www.opendota.com/" target="_blank" rel="noreferrer">OpenDota</a>, built with React and Next.js 
-				</Typography>
-        <Typography variant="body2" component="p">
-					If you are currently not a dota 2 player, but would like to see a player stats overview, my profile can be found <Link href="/players/170365079"><a>here</a></Link>
-				</Typography>
-			</Container>
+      <Container sx={{ mt: 4 }}>
+        {account}
+        <Typography component="p" mt={2}>
+          Reactive Stats is a small platform built for personal use that
+          provides a quick overview of player stats as well as information of
+          the various heroes found in the Dota 2.
+        </Typography>
+        <Typography variant="body" component="p" mt={2}>
+          This is a passion project inspired by{" "}
+          <a href="https://www.dotabuff.com/" target="_blank" rel="noreferrer">
+            DotaBuff
+          </a>{" "}
+          and{" "}
+          <a href="https://www.opendota.com/" target="_blank" rel="noreferrer">
+            OpenDota
+          </a>
+          , built with React and Next.js
+        </Typography>
+        <Typography variant="body2" component="p" mt={2}>
+          If you are currently not a dota 2 player, but would like to see a
+          player stats overview, my profile can be found{" "}
+          <Link href="/players/170365079">
+            <a>here</a>
+          </Link>
+        </Typography>
+      </Container>
     </>
   );
 }
--- pages/players/index.js
+++ pages/players/index.js
@@ -1,50 +1,64 @@
 import Link from "next/link";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import { useRouter } from 'next/router'
-import useSWR from 'swr';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import { Box } from "@mui/system";
 import Spinner from "../../components/spinner";
-
+import { Container } from "@mui/material";
 const baseUrl = "https://api.opendota.com/api/search?q=";
-const fetcher = query => fetch(baseUrl + query).then(res => res.json());
+const fetcher = (query) => fetch(baseUrl + query).then((res) => res.json());
 
 function Players() {
-	const router = useRouter();
-	const { q } = router.query;
-	const { data, error } = useSWR(q, fetcher);
-	
-	if (error) return <div>failed to load</div>
-	if(!data) return <Spinner />
-	if(data) return (
-		<Box sx={{p:3}}>
-		{data.map((player) => (
-			<Link
-				href={`/players/${player.account_id}`}
-				key={player.account_id}
-				>
-				<Card sx={{ maxWidth: 345 }}>
-					<CardActionArea>
-						<CardMedia
-							component="img"
-							height="210"
-							image={player.avatarfull}
-							alt="green iguana"
-						/>
-						<CardContent>
-							<Typography gutterBottom variant="h5" component="div">
-							{player.personaname}
-							</Typography>
-						</CardContent>
-					</CardActionArea>
-				</Card>
-			</Link>
-			))}
-	</Box>
-	);
+  const router = useRouter();
+  const { q } = router.query;
+  const { data, error } = useSWR(q, fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <Spinner />;
+  if (data)
+    return (
+      <Container
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          mt: 4,
+          gap: 2,
+          p: 5,
+        }}
+      >
+        {data.map((player) => (
+          <Box sx={{ p: 3 }} key={player.account_id}>
+            <Link href={`/players/${player.account_id}`} passHref>
+              <Card
+                sx={{ maxWidth: 345 }}
+                onClick={() => {
+                  localStorage.setItem("id", player.account_id);
+                  localStorage.setItem("selected", player.account_id);
+                }}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="210"
+                    image={player.avatarfull}
+                    alt={player.personaname}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {player.personaname}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Link>
+          </Box>
+        ))}
+      </Container>
+    );
 }
 
 export default Players;
