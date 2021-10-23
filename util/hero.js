e7c09b8a8472b007e3e01be9c21820aca714ad27
Thurston De Kock <56585531+Zwelc@users.noreply.github.com>
2021-10-23 19:37:01 +0200

    initial

--- util/hero.js
+++
@@ -1,8 +0,0 @@
-export function getAttributeIcon(attr) {
-  if (attr == "agi")
-    return "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_agility.png";
-  if (attr == "int")
-    return "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_intelligence.png";
-  if (attr == "str")
-    return "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/icons/hero_strength.png";
-}
