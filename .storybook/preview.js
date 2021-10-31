3b9bee593b203312cd0895d1bb365a3d4d502e7e
Thurston De Kock <56585531+Zwelc@users.noreply.github.com>
2021-10-31 21:06:04 +0200

    changed storybook theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,20 +7,20 @@ import { themes } from "@storybook/theming";
 const defaultTheme = createTheme({
   palette: {
     mode: "dark",
-    primary: {
-      main: "#16161a",
-      contrastText: "#dcd5d5",
-    },
-    secondary: {
-      main: "#f50057",
-    },
-    text: {
-      primary: "#adbac7",
-    },
-    background: {
-      default: "#242629",
-      paper: "#524f4f",
-    },
+    // primary: {
+    //   main: "#16161a",
+    //   contrastText: "#dcd5d5",
+    // },
+    // secondary: {
+    //   main: "#f50057",
+    // },
+    // text: {
+    //   primary: "#adbac7",
+    // },
+    // background: {
+    //   default: "#242629",
+    //   paper: "#524f4f",
+    // },
   },
 });
 
