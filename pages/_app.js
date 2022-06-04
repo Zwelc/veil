30c3fdb08f086e85599b4c27479133c2e6878fed
Thurston De Kock <56585531+Zwelc@users.noreply.github.com>
2022-06-04 13:49:08 +0200

    added veil of discord favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
