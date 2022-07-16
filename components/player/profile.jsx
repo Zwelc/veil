59bd8e9ad83b246f96d1865d3fd413bf6330cc7c
Thurston de Kock <zwelc.codes@gmail.com>
2022-07-16 16:00:27 +0200

    added profile skeleton

--- components/player/profile.jsx
+++ components/player/profile.jsx
@@ -1,31 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
-import { Avatar, Typography } from "@mui/material";
-import { Paper } from "@mui/material";
-import { Box } from "@mui/system";
-import Image from "next/image";
 import usePlayer from "../../hooks/usePlayer";
-import Spinner from "../spinner";
-const ranks = [
-  "Herald",
-  "Guardian",
-  "Crusader",
-  "Archon",
-  "Legend",
-  "Ancient",
-  "Divine",
-  "Immortal",
-];
-function getRank(tier) {
-  const text = tier.toString();
-  const index = text.slice(0, 1);
-  const rank = ranks[index - 1];
 
-  return rank + " " + text.slice(1);
-}
+import { getRank } from "../../lib/ranks";
 function Profile({ id }) {
   const { player, isLoading } = usePlayer(id);
 
-  if (isLoading) return <Spinner />;
+  if (isLoading) {
+    return (
+      <div className="flex items-center space-x-4">
+        <div className="w-24 h-24 rounded-full relative bg-gray-500 animate-pulse"></div>
+        <div className=" space-y-1 font-medium text-2xl text-white">
+          <div className="h-6 w-full bg-gray-500 animate-pulse"></div>
+          <div className="h-6 w-full bg-gray-500 animate-pulse"></div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="flex items-center space-x-4">
@@ -37,24 +27,9 @@ function Profile({ id }) {
       />
       <div className=" space-y-1 font-medium text-2xl text-white">
         <div>{player.profile.personaname}</div>
+        <div>{getRank(player.rank_tier)}</div>
       </div>
     </div>
-    // <Box
-    //   sx={{
-    //     display: "flex",
-    //     flexDirection: "column",
-    //     alignItems: "center",
-    //   }}
-    // >
-    //   <Avatar
-    //     alt={player.profile.personaname}
-    //     src={player.profile.avatarfull}
-    //     sx={{ width: 255, height: 255 }}
-    //   />
-    //   <Typography component="div" variant="h3">
-    //     {player.profile.personaname}
-    //   </Typography>
-    // </Box>
   );
 }
 
