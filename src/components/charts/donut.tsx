3b1bdaa7a8f286cd0523320ccb58179fd0ed6c63
Thurston de Kock <zwelc.codes@gmail.com>
2024-03-18 22:42:24 +0200

    fixed donut types

--- src/components/charts/donut.tsx
+++ src/components/charts/donut.tsx
@@ -1,18 +1,16 @@
+import { IPlayerSide } from "@/models/counts";
 import { VisSingleContainer, VisDonut, VisTooltip } from "@unovis/react";
 import { Donut } from "@unovis/ts";
-type Data = {
-  name: string;
-  win: number;
-  games: number;
-};
-export default function DonutComponent({ data }: { data: Data[] }) {
-  // const data: number[] = data
-  const triggers = { [Donut.selectors.segment]: (d) => d.data.name };
+
+export default function DonutComponent({ data }: { data: IPlayerSide[] }) {
+  const triggers = {
+    [Donut.selectors.segment]: (d: { data: IPlayerSide }) => d.data.name,
+  };
   return (
     <VisSingleContainer data={data}>
       <VisTooltip triggers={triggers} />
       <VisDonut
-        value={(d: Data) => d.win}
+        value={(d: IPlayerSide) => d.win}
         radius={120}
         centralLabel="Games Per Side"
       />
