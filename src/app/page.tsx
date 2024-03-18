fbf47bd210885534e8cc5266cf3ffaaa3d5aaab5
Thurston de Kock <zwelc.codes@gmail.com>
2024-03-18 18:49:37 +0200

    Updated player card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,17 +18,17 @@ export default function Home() {
     return <ProPlayers />;
   }
   return (
-    <div className="w-full h-full px-6">
+    <div className="w-full h-full px-6 py-6">
       <h1 className="w-full py-2  text-left ">
         {isLoading && "Searching..."}
-        {data && "Displaying the first 24 players"}
+        {data && "Displaying the first 16 players"}
       </h1>
-      <div className="grid grid-cols-4 gap-2  ">
+      <div className="grid grid-cols-8 gap-4  ">
         {isLoading &&
           skeleton.map((item, index) => (
             <div key={index} className="tile animate-pulse "></div>
           ))}
-        {data?.slice(0, 24).map((player: any) => (
+        {data?.slice(0, 16).map((player: any) => (
           <PlayerCard player={player} key={player.account_id} />
         ))}
       </div>
--- src/components/player-card.tsx
+++ src/components/player-card.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import Image from "next/image";
 
 export default function PlayerCard({ player }: { player: any }) {
   const router = useRouter();
@@ -14,15 +21,13 @@ export default function PlayerCard({ player }: { player: any }) {
 
   return (
     <Card className="w-full h-full">
-      <CardHeader className="flex flex-row items-center space-x-4 space-y-0 pb-2">
-        <Avatar>
-          <AvatarImage src={player.avatarfull} />
+      <CardHeader className="w-full flex items-center justify-center">
+        <Avatar className="h-32 w-32  flex items-center justify-center">
+          <AvatarImage src={player.avatarfull} className="h-32 w-32" />
           <AvatarFallback>{player.personaname}</AvatarFallback>
         </Avatar>
         <CardTitle>{player.personaname}</CardTitle>
-      </CardHeader>
-      <CardContent className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <p className="text-sm text-muted-foreground">
+        <CardDescription>
           Last Seen:{" "}
           {player.last_match_time
             ? new Date(player.last_match_time).toLocaleDateString("en-ZA", {
@@ -32,22 +37,11 @@ export default function PlayerCard({ player }: { player: any }) {
                 // day: "numeric",
               })
             : "Undisclosed"}
-        </p>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-row items-center justify-end space-y-0 pb-2">
         <Button onClick={handleView}>See More</Button>
       </CardContent>
-      {/* <CardFooter className="flex flex-row items-center justify-end space-y-0 pb-2">
-        <p className="text-sm text-muted-foreground">
-          {player.last_match_time
-            ? new Date(player.last_match_time).toLocaleDateString("en-ZA", {
-                // weekday: "long",
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })
-            : "Undisclosed"}
-        </p>
-        <Button onClick={handleView}>See More</Button>
-      </CardFooter> */}
     </Card>
   );
 }
