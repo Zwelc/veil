beffb060e1b19c59146a8ea268eca970eb645099
Thurston de Kock <zwelc.codes@gmail.com>
2024-05-24 22:41:14 +0200

    Cleaned up the totals tile

--- src/components/tiles/totals-tiles.tsx
+++ src/components/tiles/totals-tiles.tsx
@@ -3,47 +3,63 @@
 import StatTile from "@/components/tiles/stat-tile";
 import { useRecent } from "@/hooks/useRecent";
 
+import { useEffect, useState } from "react";
+
+type IRecentStat = {
+  title: string;
+  stat: string;
+  subtitle: string;
+};
 export default function TotalsTiles({ id }: { id: string }) {
   const recent = useRecent(id);
-  return (
-    <div className="w-full h-full grid grid-cols-4 col-span-2 md:col-span-4 gap-2">
-      {recent && (
-        <StatTile
-          title="Top game mode in last 20 games"
-          stat={`${recent.mostFrequentGameMode.gameMode}`}
-          subtitle={`${recent.mostFrequentGameModeMatches.length} matches played`}
-        />
-      )}
-      {recent && (
-        <StatTile
-          title="Wins in last 20 games"
-          stat={`${recent.recentWins}`}
-          subtitle={`${(
+  const [recentStats, setRecentStats] = useState([] as IRecentStat[]);
+
+  useEffect(() => {
+    if (recent) {
+      setRecentStats([
+        {
+          title: "Top game mode in last 20 games",
+          stat: recent.mostFrequentGameMode.gameMode,
+          subtitle: `${recent.mostFrequentGameModeMatches.length} matches played`,
+        },
+        {
+          title: "Wins in last 20 games",
+          stat: `${recent.recentWins}`,
+          subtitle: `${(
             (recent.recentWins / recent.recentMatches.length) *
             100
-          ).toFixed(2)} % Winrate`}
-        />
-      )}
-      {recent && (
-        <StatTile
-          title="Most Frequent Hero in last 20 games"
-          stat={`${recent.mostFrequentHero.name}`}
-          subtitle={`${(
+          ).toFixed(2)} % Winrate`,
+        },
+        {
+          title: "Most Frequent Hero in last 20 games",
+          stat: `${recent.mostFrequentHero.name}`,
+          subtitle: `${(
             (recent.mostFrequentHeroMatches.filter(
               (match) => match.result == "Won"
             ).length /
               recent.mostFrequentHeroMatches.length) *
             100
-          ).toFixed(2)} % Winrate`}
-        />
-      )}
-      {recent && (
+          ).toFixed(2)} % Winrate`,
+        },
+        {
+          title: `${recent.mostFrequentHero.name}'s KDA in last 20 games`,
+          stat: `${recent.mostFrequentHeroKDA}`,
+          subtitle: `Across ${recent.mostFrequentHeroMatches.length} matches`,
+        },
+      ]);
+    }
+  }, [recent]);
+
+  return (
+    <div className="w-full h-full grid grid-cols-4 col-span-2 md:col-span-4 gap-2">
+      {recentStats.map((stat) => (
         <StatTile
-          title={`${recent.mostFrequentHero.name}'s KDA in last 20 games`}
-          stat={`${recent.mostFrequentHeroKDA}`}
-          subtitle={`Across ${recent.mostFrequentHeroMatches.length} matches`}
+          key={stat.title}
+          title={stat.title}
+          stat={stat.stat}
+          subtitle={stat.subtitle}
         />
-      )}
+      ))}
     </div>
   );
 }
