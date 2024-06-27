2319bef45b96fb94e346bda1cd770bc77d216583
Zwelc <zwelc.codes@gmail.com>
2024-06-27 19:16:51 +0200

    Updated counter tile skeleton

--- src/components/tiles/counter-tiles.tsx
+++ src/components/tiles/counter-tiles.tsx
@@ -2,46 +2,53 @@ import { IPlayerParam } from "@/models/player.model";
 import { useRecentStats } from "@/hooks/useRecent";
 import { usePlayerWinrate } from "@/hooks/usePlayer";
 import StatTile from "./stat-tile";
+import { Skeleton } from "../ui/skeleton";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 export default function CounterTiles({ id }: IPlayerParam) {
   const { data, isLoading } = usePlayerWinrate(id);
   const recentStats = useRecentStats(id);
   const skeleton = new Array(6).fill(null);
 
-  if (isLoading) {
-    return (
-      <div className="row-span-12 md:row-span-1 md:col-span-6 col-span-2">
-        {skeleton.map((item, index) => (
-          <div
-            key={index}
-            className="tile animate-pulse flex flex-col items-center justify-center"
-          >
-            <div className="animate-pulse h-2 bg-gray-200 rounded-full  max-w-[48px] mb-2.5"></div>
-            <div className="animate-pulse h-2 bg-gray-200 max-w-[16px]"></div>
-          </div>
-        ))}
-      </div>
-    );
-  }
   return (
     <div className="row-span-12 md:row-span-1 md:col-span-6 col-span-2">
       <div className="w-full h-full grid grid-cols-2 md:grid-cols-6 gap-2">
-        <StatTile
-          title="Total Games"
-          stat={data.win + data.lose}
-          subtitle={`${((data.win / (data.win + data.lose)) * 100).toFixed(
-            2
-          )} % Winrate`}
-        />
-        <StatTile title="Total Wins" stat={data.win} subtitle={`Overall`} />
-        {recentStats.map((stat) => (
-          <StatTile
-            key={stat.title}
-            title={stat.title}
-            stat={stat.stat}
-            subtitle={stat.subtitle}
-          />
-        ))}
+        {isLoading ? (
+          skeleton.map((item, index) => (
+            <Card key={index}>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  <Skeleton className="h-4 w-[150px]" />
+                </CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-2">
+                  <Skeleton className="h-4 w-[200px]" />
+                  <Skeleton className="h-4 w-[140px]" />
+                </div>
+              </CardContent>
+            </Card>
+          ))
+        ) : (
+          <>
+            <StatTile
+              title="Total Games"
+              stat={data.win + data.lose}
+              subtitle={`${((data.win / (data.win + data.lose)) * 100).toFixed(
+                2
+              )} % Winrate`}
+            />
+            <StatTile title="Total Wins" stat={data.win} subtitle={`Overall`} />
+            {recentStats.map((stat) => (
+              <StatTile
+                key={stat.title}
+                title={stat.title}
+                stat={stat.stat}
+                subtitle={stat.subtitle}
+              />
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
